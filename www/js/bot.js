2c2a8668646ac277ef2ea601b67a4cff57cfe8f9
paulBeaudet <inof8or@gmail.com>
2016-04-03 14:52:18 -0400

    clean up the code, make sure it will work regardless of video capability

--- www/js/bot.js
+++ www/js/bot.js
@@ -1,12 +1,11 @@
 // bot.js ~ Copyright 2016 Paul Beaudet ~ MIT License
-var SERVER = 'https://telezumo.herokuapp.com';
+var SERVER = 'https://telezumo.herokuapp.com'; // add YOUR server here
 
 var app = {
     initialize: function(){    // Application Constructor
         this.bindEvents();     // Bind Event Listeners
-        $('.navbar').hide();   // hide navbar by defualt
         $('#sConnect').hide(); // hide connection button until device is ready
-        $('#remote').hide();   // hide remote connection button
+        $('#refresh').hide();  // hide remote connection refresh button (socket.io)
         $('#videoBTN').hide(); // hide video button until video shows up
     },
     bindEvents: function() {   // Bind cordova events: 'load', 'deviceready', 'offline', and 'online'
@@ -16,7 +15,6 @@ var app = {
         sock.connect();                                 // socket connect event
         video.init();                                   // get bot video stream
         $('#sConnect').show().on('click', arduino.ask); // on click ask if we can connect to the arduino
-        $('#sendButton').on('click', arduino.send);     // send typed data in textEntry space
     }
 };
 
@@ -29,22 +27,16 @@ var arduino = {
             $('#sConnect').off().text('close').on('click', arduino.close);
             serial.registerReadCallback(arduino.read, utils.error);       // set read callback
             $('#status').text(msg);                                       // show status of serial port
-            $('.navbar').show();                                          // show navbar to send serial data
         }, utils.error);
-    },
-    send: function(){
-        var text = $('#textEntry').val();
-        if(text){
-            serial.write(text, function(){$('#textEntry').val('');}, utils.error);
-        } else {$('#status').text('nothing entered');}
     },
     read: function(data){
         var view = new Uint8Array(data);
         if(view.length){
             for(var i=0; i < view.length; i++){
                 if(view[i] == 13){
-                    $('#output').text(arduino.tempIn);  // print serialln
-                    arduino.tempIn = '';                // reset temp in
+                    $('#output').text(arduino.tempIn);    // print serialln
+                    sock.et.emit('data', arduino.tempIn); // relay read data to server
+                    arduino.tempIn = '';                  // reset temp in
                 } else {
                     arduino.tempIn += unescape(escape(String.fromCharCode(view[i])));
                 }
@@ -54,7 +46,6 @@ var arduino = {
     close: function(){
         serial.close(function(){
             $('#status').text('closed serial port');
-            $('.navbar').hide();
             $('#sConnect').off().text('connect').on('click', arduino.ask); // onclick ask if we can connect to the arduino
         }, utils.error);
     },
@@ -72,23 +63,21 @@ var signal = {
     peer: null,
     peerConnect: function(amIfirst){
         signal.peer = new window.RTCPeerConnection({ 'iceServers': [{'url': 'stun:stun.l.google.com:19302'}] });
-        signal.peer.onicecandidate = function (event) { // send any ice candidates to the other peer
-            if (event.candidate != null) {
-                sock.et.emit('ice', JSON.stringify(event.candidate));
-            } // else a null means we have finished finding ice canidates in which there
-        };    // in which there may be multiple of for any given client
-        signal.peer.onaddstream = video.remoteStream;
-        signal.peer.addStream(video.stream);
-        if(amIfirst) { signal.peer.createOffer(signal.onSession, utils.error);}
+        signal.peer.onicecandidate = function (event) { // on address info being introspected from external "stun" server
+            if (event.candidate != null) { sock.et.emit('ice', JSON.stringify(event.candidate)); }
+        }; // null === finished finding info to describe ones own address, ie "canidate" address paths
+        signal.peer.onaddstream = video.remoteStream;  // display remote video stream when it comes in
+        signal.peer.addStream(video.stream);           // make our video stream sharable
+        if(amIfirst){ signal.peer.createOffer(signal.onSession, utils.error);}
     },
     recepient: function(info, type){
         if(!signal.peer){signal.peerConnect(false);} // start peer connection if someone is calling
-        if(type === 'ice'){
-            signal.peer.addIceCandidate(new window.RTCIceCandidate(JSON.parse(info)));
-        } else {
+        if(type === 'ice'){                          // given adress info from remote is being handled
+            signal.peer.addIceCandidate(new window.RTCIceCandidate(JSON.parse(info))); // add address info
+        } else { // otherwise we are getting signal type data i.e. audeo video codec description
             signal.peer.setRemoteDescription(new window.RTCSessionDescription(JSON.parse(info)), function(){
                 signal.peer.createAnswer(signal.onSession, utils.error);
-            });
+            }, utils.error); // try to find common ground on codecs
         }
     },
     onSession: function(info){
@@ -101,37 +90,34 @@ var signal = {
 var video = {
     stream: null,
     init: function(){
-        navigator.getUserMedia({video: true, audio: false,}, function(stream){
-            video.stream = stream;
-            document.getElementById('localVid').src = window.URL.createObjectURL(stream);
-            $('#videoBTN').show().on('click', function(){signal.peerConnect(true);});
-        }, utils.error);
+        if(navigator.getUserMedia){
+            navigator.getUserMedia({video: true, audio: true,}, function(stream){
+                video.stream = stream;
+                $('#videoBTN').show().on('click', function(){signal.peerConnect(true);});
+            }, utils.error);
+        } else {
+            utils.error('Telepresence, not supported on this device');
+        }
     },
     remoteStream: function(event){
         document.getElementById('remoteVid').src = window.URL.createObjectURL(event.stream);
     }
 }
 
-var utils = {
-    error: function(err){$('#status2').text('error:'+err);}
-}
+var utils = {error: function(err){$('#status2').text('error:'+err);}}
 
 sock = {
     et: false,  // need to try to connect before getting to excited
     connect: function(){
-        try {sock.et = io.connect(SERVER);}
-        catch(err){
-            $('#remote').text('retry').show().on('click', sock.connect);
-            alert(err);
-        }
-        sock.et.on('connect', sock.init);
-        // Signaling reactions
-        sock.et.on('ice', function(info){signal.recepient(info, 'ice');});
-        sock.et.on('sdp', function(info){signal.recepient(info, 'sdp');});
+        try {sock.et = io.connect(SERVER);}                                        // see if server is available
+        catch(err){$('#refresh').text('retry').show().on('click', sock.connect);}  // if not get option to refresh
+        sock.et.on('connect', sock.init);                                          // on connection set event listeners
     },
     init: function(){
-        $('#remote').hide();
-        sock.et.on('remote', arduino.remote);
+        $('#refresh').hide();
+        sock.et.on('remote', arduino.remote);                              // relay remote control events
+        sock.et.on('ice', function(info){signal.recepient(info, 'ice');}); // get ip information
+        sock.et.on('sdp', function(info){signal.recepient(info, 'sdp');}); // get video codec information
     },
 }
 
