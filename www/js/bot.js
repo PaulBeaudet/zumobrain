73014b2ad422a0a92bcd621ec0eae0cf6aba29a8
paulBeaudet <inof8or@gmail.com>
2016-04-04 14:10:35 -0400

    experiment with being controled by only one remote

--- www/js/bot.js
+++ www/js/bot.js
@@ -34,9 +34,9 @@ var arduino = {
         if(view.length){
             for(var i=0; i < view.length; i++){
                 if(view[i] == 13){
-                    $('#output').text(arduino.tempIn);    // print serialln
-                    sock.et.emit('data', arduino.tempIn); // relay read data to server
-                    arduino.tempIn = '';                  // reset temp in
+                    $('#output').text(arduino.tempIn);  // print serialln
+                    sock.send('data', arduino.tempIn);  // relay read data to server
+                    arduino.tempIn = '';                // reset temp in
                 } else {
                     arduino.tempIn += unescape(escape(String.fromCharCode(view[i])));
                 }
@@ -64,7 +64,7 @@ var signal = {
     peerConnect: function(amIfirst){
         signal.peer = new window.RTCPeerConnection({ 'iceServers': [{'url': 'stun:stun.l.google.com:19302'}] });
         signal.peer.onicecandidate = function (event) { // on address info being introspected from external "stun" server
-            if (event.candidate != null) { sock.et.emit('ice', JSON.stringify(event.candidate)); }
+            if (event.candidate != null) { sock.send('ice', JSON.stringify(event.candidate)); }
         }; // null === finished finding info to describe ones own address, ie "canidate" address paths
         signal.peer.onaddstream = video.remoteStream;  // display remote video stream when it comes in
         signal.peer.addStream(video.stream);           // make our video stream sharable
@@ -82,7 +82,7 @@ var signal = {
     },
     onSession: function(info){
         signal.peer.setLocalDescription(info, function(){
-            sock.et.emit('sdp', JSON.stringify(signal.peer.localDescription)); // send discription of connection type
+            sock.send('sdp', JSON.stringify(signal.peer.localDescription)); // send discription of connection type
         }, utils.error);
     },
 }
@@ -107,7 +107,9 @@ var video = {
 var utils = {error: function(err){$('#status2').text('error:'+err);}}
 
 sock = {
-    et: false,  // need to try to connect before getting to excited
+    et: false,       // need to try to connect before getting to excited
+    status: 'open',  // detects whether bot is being controled or not
+    master: null,    // user allowed to control this bot
     connect: function(){
         try {sock.et = io.connect(SERVER);}                                        // see if server is available
         catch(err){$('#refresh').text('retry').show().on('click', sock.connect);}  // if not get option to refresh
@@ -115,9 +117,22 @@ sock = {
     },
     init: function(){
         $('#refresh').hide();
+        sock.et.on('botFind', function(from){
+            sock.et.emit('here', {id:from, status: sock.status});
+        });
+        sock.et.on('own', function(from){
+            if(sock.master !== from){  // relinquish control case
+                sock.master = from;    // robot's master is defined!
+                sock.status = 'taken'; // denote robot is being controled for admins
+                sock.et.emit('here', {status: 'taken'}); // broadcast bot has been taken
+            }
+        });
         sock.et.on('remote', arduino.remote);                              // relay remote control events
         sock.et.on('ice', function(info){signal.recepient(info, 'ice');}); // get ip information
         sock.et.on('sdp', function(info){signal.recepient(info, 'sdp');}); // get video codec information
+    },
+    send: function(type, data){
+        if(sock.master){sock.et.emit(type, {to:sock.master, data:data});}
     },
 }
 
