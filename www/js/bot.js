94366e3caede76eb8388264a02b6ca7b45b62ea5
paulBeaudet <inof8or@gmail.com>
2016-04-06 11:53:53 -0400

    Strenthen up comments and default ask for usb permissions

--- www/js/bot.js
+++ www/js/bot.js
@@ -1,58 +1,49 @@
 // bot.js ~ Copyright 2016 Paul Beaudet ~ MIT License
-var SERVER = 'http://192.168.1.84:3000'; // test on your local server
-// var SERVER = 'https://telezumo.herokuapp.com'; // or add YOUR server here
+// var SERVER = 'http://192.168.1.84:3000'; // test on YOUR local server here
+var SERVER = 'https://telezumo.herokuapp.com'; // or add YOUR server here
 
-var app = {
-    initialize: function(){    // Application Constructor
-        this.bindEvents();     // Bind Event Listeners
-        $('#sConnect').hide(); // hide connection button until device is ready
-        $('#refresh').hide();  // hide remote connection refresh button (socket.io)
-        $('#videoBTN').hide(); // hide video button until video shows up
-    },
-    bindEvents: function() {   // Bind cordova events: 'load', 'deviceready', 'offline', and 'online'
-        document.addEventListener('deviceready', this.onDeviceReady, false); // function to run when device ready
-    },
-    onDeviceReady: function() {                         // deviceready Event Handler
-        sock.connect();                                 // socket connect event
-        $('#sConnect').show().on('click', arduino.ask); // on click ask if we can connect to the arduino
-    }
-};
-
-var arduino = {
+var arduino = {  // code for controling the arduino, NOTE: braided w/socket events
     tempIn: '',  // empty string to hold incoming serial data
-    ask: function(){serial.requestPermission(arduino.open, utils.error);},
+    ask: function(){serial.requestPermission(arduino.open, function(err){ // if permission granted open serial port
+        utils.error(err);                                                                        // else show error
+        $('#sConnect').show().off().text('USB pemission needed:Retry').on('click', arduino.ask); // try again option
+    });},
     open: function(){
-        $('#sConnect').text('retry');
-        serial.open({baudRate: 9600, sleepOnPause: false}, function(msg){
-            $('#sConnect').off().text('close').on('click', arduino.close);
-            serial.registerReadCallback(arduino.read, utils.error);       // set read callback
-            $('#status').text(msg);                                       // show status of serial port
-        }, utils.error);
+        serial.open({baudRate: 9600, sleepOnPause: false}, function(msg){        // provide settings and success callback
+            sock.et.emit('here', {id:false, status:'open'});                     // broadcast readyness to remotes
+            $('#sConnect').show().off().text('stop').on('click', arduino.close); // give ability to close connection
+            serial.registerReadCallback(arduino.read, utils.error);              // set read callback
+            $('#status').text(msg);                                              // show status of serial port
+        }, function(err){                                                        // on failed to open callback
+            $('#sConnect').show().off().text('Failed talking to bot:Retry').on('click', arduino.open);
+            utils.error(err);                                                    // log out fail message
+        });
     },
-    read: function(data){
-        var view = new Uint8Array(data);
-        if(view.length){
-            for(var i=0; i < view.length; i++){
-                if(view[i] == 13){
-                    $('#output').text(arduino.tempIn); // output serialln
-                    sock.send('data', arduino.tempIn); // relay read data to server
-                    arduino.tempIn = '';               // reset temp in
-                } else {
-                    arduino.tempIn += unescape(escape(String.fromCharCode(view[i])));
+    read: function(rawBuffer){                         // read incoming data from the arduino
+        var buffer = new Uint8Array(rawBuffer);        // turn raw buffer into something we can understand
+        if(buffer.length){                             // if there are bytes in buffer
+            for(var i=0; i < buffer.length; i++){      // for bytes that we got
+                if(buffer[i] == 13){                   // if on new line byte
+                    $('#output').text(arduino.tempIn); //   output serialln
+                    sock.send('data', arduino.tempIn); //   relay read data to server
+                    arduino.tempIn = '';               //   reset temp in
+                } else {                               // if case of any other byte add it to a pesistent string
+                    arduino.tempIn += unescape(escape(String.fromCharCode(buffer[i])));
                 }
             }
         }
     },
     close: function(){
         serial.close(function(){
-            $('#status').text('closed serial port');
+            $('#status').text('closed serial port and remote connections');// show what just happend
+            sock.et.emit('here', {id:false, status:'taken'});              // broadcast taken status to remotes
             $('#sConnect').off().text('connect').on('click', arduino.ask); // onclick ask if we can connect to the arduino
         }, utils.error);
     },
     remote: function(data){serial.write(data, function(){$('#status2').text('sent ' + data);}, utils.error);}
 }
 
-// simplified adapter.js shims for webRTC browser differances
+// simplified adapter.js shims for webRTC browser differances AKA bs garble de gook
 navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
 window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
@@ -60,7 +51,7 @@ window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSess
 window.URL = window.URL || window.webkitURL;
 
 var signal = {
-    peer: null,
+    peer: null, // placeholder for our peer connection object
     peerConnect: function(amIfirst){
         signal.peer = new window.RTCPeerConnection({ 'iceServers': [{'url': 'stun:stun.l.google.com:19302'}] });
         signal.peer.onicecandidate = function (event) { // on address info being introspected from external "stun" server
@@ -104,32 +95,25 @@ var video = {
 
 var utils = {error: function(err){$('#status2').text('error:'+err);}}
 
-sock = {
+sock = {             // socket.io event listeners and variables for bot status
     et: false,       // need to try to connect before getting to excited
-    status: 'open',  // detects whether bot is being controled or not
+    status: 'taken', // detects whether bot is being controled or not
     master: null,    // user allowed to control this bot
-    connect: function(){
-        try {sock.et = io.connect(SERVER);}                                        // see if server is available
-        catch(err){$('#refresh').text('retry').show().on('click', sock.connect);}  // if not get option to refresh
-        sock.et.on('connect', sock.init);                                          // on connection set event listeners
-    },
     init: function(){
-        $('#refresh').hide();
-        sock.et.emit('here', {id:false, status:'open'}); // broadcast availibility to masters that are already online
         sock.et.on('botFind', function(from){sock.et.emit('here', {id:from, status: sock.status});});
         sock.et.on('own', function(from){
             if(sock.master !== from){  // relinquish control case
                 video.init();          // give ability to connect video
-                sock.master = from;    // robot's master is defined!
+                sock.master = from;    // robot's master is defined! yousa gone save my life!
                 sock.status = 'taken'; // denote robot is being controled for admins
                 sock.et.emit('here', {id:false, status:'taken'}); // broadcast bot has been taken
             }
         });
-        sock.et.on('relinquish', function(master){
-            if(master = sock.master){
-                sock.master = null;
-                sock.status = 'open';
-                sock.et.emit('here', {id:false, status: sock.status});
+        sock.et.on('relinquish', function(master){ // NOTE this is a broadcasted event we are listening for
+            if(master = sock.master){  // check if this is our master that has bared the gift of a sock
+                sock.master = null;    // Dolby is FREEEE!
+                sock.status = 'open';  // set status persitently so we can respond to indivdual botFind calls
+                sock.et.emit('here', {id:false, status: 'open'});     // broadcast newfound freedom
             }
         });
         sock.et.on('remote', arduino.remote);                              // relay remote control events
@@ -141,4 +125,24 @@ sock = {
     },
 }
 
-app.initialize();
+var app = {                    // event listeners for cordova
+    initialize: function(){    // Application Constructor
+        this.bindEvents();     // Bind Event Listeners
+        $('#sConnect').hide(); // hide connection button until device is ready (arduino)
+        $('#refresh').hide();  // hide remote connection refresh button (socket.io)
+    },
+    bindEvents: function() {   // Bind cordova events: 'load', 'deviceready', 'offline', and 'online'
+        document.addEventListener('deviceready', app.start); // function to run when device ready
+    },
+    start: function(){
+        try {sock.et = io.connect(SERVER);}                                    // see if server is available
+        catch(err){$('#refresh').text('retry').show().on('click', app.start);} // if not get option to refresh
+        sock.et.on('connect', function(){                                      // on succesfull connection to server
+            $('#refresh').hide();                                              // hide refresh button if it was shown
+            sock.init();                                                       // set event listeners
+            arduino.ask();                                                     // ask for permission to use USB
+        });
+    }
+};
+
+app.initialize(); // read upwards app starts here
